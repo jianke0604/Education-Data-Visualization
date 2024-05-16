428035c1c65fcd29d84b24609132159a622a76ae
etherwindy <ether-wind@sjtu.edu.cn>
2024-05-16 18:38:37 +0800

    update DateHeatMap

--- data/dateheatmap.py
+++ data/dateheatmap.py
@@ -8,6 +8,18 @@ input_files = [f'./Data_SubmitRecord/SubmitRecord-Class{i}.csv' for i in range(1
 output_file = 'dateheatmap.json'
 
 # 创建一个空的列表来存储处理后的数据
+knowledge_map = {}
+knowldege_list = []
+
+with open('Data_TitleInfo.csv', mode='r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        knowledge_map[row['title_ID']] = {'knowledge': row['knowledge'], 'sub_knowledge': row['sub_knowledge']}
+        if row['knowledge'] not in knowldege_list:
+            knowldege_list.append(row['knowledge'])
+        if row['sub_knowledge'] not in knowldege_list:
+            knowldege_list.append(row['sub_knowledge'])
+        
 data = {}
 
 # 读取每个CSV文件并处理
@@ -25,10 +37,19 @@ for input_file in input_files:
             
             # 判断日期是否已经存在
             if answer_time not in data:
-                data[answer_time] = 0
+                data[answer_time] = {'total': 1}
+                for item in knowldege_list:
+                    data[answer_time][item] = 0
             
             else:
-                data[answer_time] += 1
+                data[answer_time]['total'] += 1
+            
+            knowledge = knowledge_map[row['title_ID']]['knowledge']
+            sub_knowledge = knowledge_map[row['title_ID']]['sub_knowledge']
+            
+            data[answer_time][knowledge] += 1
+            data[answer_time][sub_knowledge] += 1
+            
 
 # 获取最早和最晚的日期
 # 获取最早和最晚的日期
@@ -41,11 +62,12 @@ date_list = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in rang
 # 将缺失的日期添加到数据中
 for date in date_list:
     if date not in data:
-        data[date] = 0
+        data[date] = {'total': 0}
+        for item in knowldege_list:
+            data[date][item] = 0
 
 # 将数据转换为列表
-data = [{'date': key, 'value': value} for key, value in data.items()]
-
+data = [{'date': key, **value} for key, value in data.items()]
 
 # 按日期排序  
 data = sorted(data, key=lambda x: x['date'])
--- src/components/DateHeatMap.vue
+++ src/components/DateHeatMap.vue
@@ -1,113 +1,149 @@
 <template>
-  <div id="dateheatmap" style="height: 600px; width: 100%;"></div>
+  <div id="submit" style="height: 200px; width: 100%"></div>
+  <div id="correct" style="height: 200px; width: 100%"></div>
+  <select id="select" v-model="knowledge"></select>
 </template>
 
 <script>
-
-
-import * as echarts from 'echarts';
+import * as echarts from "echarts";
 
 export default {
-  name: 'DateHeatMap',
+  name: "DateHeatMap",
+  data() {
+    return {
+      submitChart: null,
+      submitOption: null,
+    };
+  },
   mounted() {
-    var chartDom = document.getElementById('dateheatmap');
-    var myChart = echarts.init(chartDom);
-    var option;
+    this.submitChart = echarts.init(document.getElementById("submit"));
 
-    function draw(original_data) {
+    document.getElementById("select").addEventListener("change", (event) => {
+      fetch("../../data/dateheatmap.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.draw(data, event.target.value);
+        });
+    });
+
+    fetch("../../data/dateheatmap.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.draw(data, 'total');
+        for (let key in data[0]) {
+          if (key === 'date') continue;
+          var option = document.createElement("option");
+          option.text = key;
+          option.value = key;
+          document.getElementById("select").appendChild(option);
+        }
+      });
+  },
+  methods: {
+    draw(original_data, knowledge) {
+
       var data = [];
       for (let i = 0; i < original_data.length; i++) {
-        data.push([
-          original_data[i].date,
-          original_data[i].value
-        ]);
+        data.push([original_data[i].date, original_data[i][knowledge]]);
       }
 
       var start_date = original_data[0].date;
       var end_date = original_data[original_data.length - 1].date;
-      var max_value = Math.max.apply(null, original_data.map(function(item) {
-        return item.value;
-      }));
+      var submit_max_value = Math.max.apply(
+        null,
+        original_data.map(function (item) {
+          return item[knowledge];
+        })
+      );
 
-      console.log(data.map(function(item) {
-        return item[1];
-      }));
-      
-      option = {
+      console.log(
+        data.map(function (item) {
+          return item[1];
+        })
+      );
+
+      this.submitOption = {
+        title: {
+          text: "提交次数",
+          left: "center",
+          top: 0,
+          textStyle: {
+            color: "black",
+            fontSize: 20,
+          },
+        },
         visualMap: {
           show: false,
           min: 0,
-          max: max_value,
+          max: submit_max_value,
           inRange: {
-            color: ['white', 'green']
-          }
+            color: ["white", "green"],
+          },
         },
         calendar: {
-          top: 'middle',
+          top: 40,
           left: 100,
           range: [start_date, end_date],
           cellSize: [20, 20],
           splitLine: {
             show: true,
             lineStyle: {
-              color: 'green',
+              color: "green",
               width: 1,
-              type: 'solid',
+              type: "solid",
             },
           },
           dayLabel: {
-            nameMap: ['', 'Mon', '', 'Wen', '', 'Fri', ''],
+            nameMap: ["", "Mon", "", "Wen", "", "Fri", ""],
           },
           yearLabel: {
             show: false,
           },
           itemStyle: {
             borderWidth: 5,
-            borderColor: 'rgba(0, 0, 0, 0)',
-
+            borderColor: "rgba(0, 0, 0, 0)",
           },
         },
         series: {
-          type: 'heatmap',
-          coordinateSystem: 'calendar',
-          data: data,
+          type: "heatmap",
+          coordinateSystem: "calendar",
+          data: data.map(function (item) {
+            return [item[0], item[1]];
+          }),
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
         },
         tooltip: {
-          position: 'top',
-          formatter: function(params) {
-            return '日期: ' + params.value[0] + '<br>' + '提交次数: ' + params.value[1];
-          }
+          position: "top",
+          formatter: function (params) {
+            return (
+              "日期: " +
+              params.value[0] +
+              "<br>" +
+              "提交次数: " +
+              params.value[1]
+            );
+          },
         },
         visualMap: {
           min: 0,
-          max: max_value,
+          max: submit_max_value,
           calculable: true,
-          orient: 'vertical',
+          orient: "vertical",
           left: 0,
-          top: 'center',
+          top: 40,
           inRange: {
-            color: ['white', 'green']
+            color: ["white", "green"],
           },
         },
       };
 
-      option && myChart.setOption(option);
-
-    }
-
-    fetch('../../data/dateheatmap.json')
-      .then(response => response.json())
-      .then(data => {
-        draw(data);
-      });
-
-    }
-}
-
+      this.submitOption && this.submitChart.setOption(this.submitOption);
+    },
+  },
+};
 </script>
